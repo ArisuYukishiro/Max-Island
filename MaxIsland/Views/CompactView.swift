035def5bd3377dacd252784e49fd6d6d955950d9
ArisuSak <120944868+ArisuSak@users.noreply.github.com>
2025-12-16 17:25:47 +0700

    chore: remove unused code

--- MaxIsland/Views/CompactView.swift
+++ MaxIsland/Views/CompactView.swift
@@ -2,11 +2,7 @@ import SwiftUI
 
 struct CompactView: View {
     var body: some View {
-        HStack(spacing: 6) {
-//            Circle()
-//                .fill(Color.green)
-//                .frame(width: 12, height: 12)
-        }
+        HStack(spacing: 6) {}
         .transition(.scale.combined(with: .opacity))
     }
 }
