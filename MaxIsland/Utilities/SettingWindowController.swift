43b3581e8388537ac0237124058dffbab14bbe47
ArisuSak <120944868+ArisuSak@users.noreply.github.com>
2025-12-16 17:46:00 +0700

    chore: add tranparent to setting window title

--- MaxIsland/Utilities/SettingWindowController.swift
+++ MaxIsland/Utilities/SettingWindowController.swift
@@ -5,7 +5,7 @@ class SettingWindowController: NSWindowController {
     convenience init() {
         let settingsWindow = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 700, height: 400),
-            styleMask: [.titled, .closable, .resizable],
+            styleMask: [.titled, .closable, .resizable, .fullSizeContentView],
             backing: .buffered,
             defer: false
         )
@@ -13,6 +13,8 @@ class SettingWindowController: NSWindowController {
         settingsWindow.center()
         settingsWindow.minSize = NSSize(width: 500, height: 350)
         settingsWindow.maxSize = NSSize(width: 1200, height: 800)
+        settingsWindow.titlebarAppearsTransparent = true
+
         
         let contentView = NSHostingView(rootView: SettingView(isPresented: .constant(true)))
         settingsWindow.contentView = contentView
